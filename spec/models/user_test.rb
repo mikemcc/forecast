22cf9e942b00a186517afdcbe0287cf5bd1b669a
Michael McCafferty <mikemcc@darkandbrooding.com>
2017-07-21 12:22:44 -0600

    intentionally failing rspec test .

---
+++ spec/models/user_test.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  context "with 2 or more comments" do
+    it "orders them in reverse chronologically" do
+      user = User.create!
+      comment1 = user.comments.create!(:body => "first comment")
+      comment2 = user.comments.create!(:body => "second comment")
+      expect(user.reload.comments).to eq([comment2, comment1])
+    end
+  end
+end
